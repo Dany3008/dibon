aa262e40766d214b5c5e25e3879ef962a4d66635
MariaAlex183 <145379379+MariaAlex183@users.noreply.github.com>
2023-10-15 12:17:10 -0500

    Update CrearUsuarios.js

--- frontend/src/components/Auth/CrearUsuarios.js
+++ frontend/src/components/Auth/CrearUsuarios.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, {useState} from "react";
 
 export const CrearUsuarios = () =>
 {
@@ -16,17 +16,26 @@ export const CrearUsuarios = () =>
     const [tipoDocumentoEspecialista, setTipoDocumentoEspecialista] = useState('');
     const [numeroDocumentoEspecialista, setNumeroDocumentoEspecialista] = useState('');
     const [valorSalarial, setValorSalarial] = useState('');
+    const [nombreOperador, setNombreOperador] = useState('');
+    const [tipoOperador, setTipoOperador] = useState('');
+    const [tipoDocumentoOperador, setTipoDocumentoOperador] = useState('');
+    const [numeroDocumentoOperador, setNumeroDocumentoOperador] = useState('');
+    const [valorSalarialOperador, setValorSalarialOperador] = useState('');
     const handleSubmit = (e) => {
         e.preventDefault();
         // Aquí puedes procesar los datos del formulario, por ejemplo, enviarlos a un servidor.
-        console.log('Datos del formulario:', { nombre, tipoDocumento, numeroDocumento, numeroContacto, correoEmpresarial });
+        console.log('Datos del formulario:', { nombreCompañia, razonSocial, nit, actividadComercial, 
+          nombre, tipoDocumento, numeroDocumento, numeroContacto, correoEmpresarial, 
+          nombreEspecialista, tipoEspecialista, tipoDocumentoEspecialista, numeroDocumentoEspecialista, valorSalarial, 
+          nombreOperador, tipoOperador, tipoDocumentoOperador, numeroDocumentoOperador, valorSalarialOperador});
       };
     return(
-        <div>
+        <div className="contenedor-formulario">
         <h2>Portal de Creacion de Usuario</h2>
+        <div className="formulario-compañia">
         <h3>Informacion de la Compañia</h3>
         <form onSubmit={handleSubmit}>
-          <div>
+          <div className="nombre-compañia">
             <label htmlFor="nombreCompañia">Nombre de la Compañia:</label>
             <input
               type="text"
@@ -36,7 +45,7 @@ export const CrearUsuarios = () =>
               required
             />
           </div>
-          <div>
+          <div className="razon-social">
             <label htmlFor="razonSocial">Razon Social:</label>
             <input
             text="text"
@@ -47,7 +56,7 @@ export const CrearUsuarios = () =>
             >
             </input>
           </div>
-          <div>
+          <div className="nit">
             <label htmlFor="nit">Nit:</label>
             <input
               type="number"
@@ -57,7 +66,7 @@ export const CrearUsuarios = () =>
               required
             />
           </div>
-          <div>
+          <div className="actividad-comercial">
             <label htmlFor="actividadComercial">Actividad Comercial:</label>
             <input
               type="text"
@@ -67,11 +76,13 @@ export const CrearUsuarios = () =>
               required
             />
           </div>
-          <button type="submit">Crear Compañia</button>
+          <button className="boton-compañia" type="submit">Crear Compañia</button>
         </form>
+        </div>
+        <div className="formulario-representante">
         <h3>Informacion del Representante Legal</h3>
         <form onSubmit={handleSubmit}>
-          <div>
+          <div className="nombre-representante">
             <label htmlFor="nombre">Nombre:</label>
             <input
               type="text"
@@ -81,7 +92,7 @@ export const CrearUsuarios = () =>
               required
             />
           </div>
-          <div>
+          <div className="tipo-documento">
             <label htmlFor="tipoDocumento">Tipo de Documento:</label>
             <select
               id="tipoDocumento"
@@ -96,7 +107,7 @@ export const CrearUsuarios = () =>
               <option value="Carnet de Extranjería">Carnet de Extranjería</option>
             </select>
           </div>
-          <div>
+          <div className="cedeula-representante">
             <label htmlFor="numeroDocumento">Número de Documento:</label>
             <input
               type="text"
@@ -106,7 +117,7 @@ export const CrearUsuarios = () =>
               required
             />
           </div>
-          <div>
+          <div className="numero-contacto">
             <label htmlFor="numeroContacto">Número de Contacto:</label>
             <input
               type="text"
@@ -116,7 +127,7 @@ export const CrearUsuarios = () =>
               required
             />
           </div>
-          <div>
+          <div className="correo-empresarial">
             <label htmlFor="correoEmpresarial">Correo Empresarial:</label>
             <input
               type="email"
@@ -126,8 +137,134 @@ export const CrearUsuarios = () =>
               required
             />
           </div>
-          <button type="submit">Crear Representante Legal</button>
+          <button className="boton-representante" type="submit">Crear Representante Legal</button>
         </form>
+        </div>
+        <div className="formulario-especialista">
+        <h3>Informacion del especialista</h3>
+        <form onSubmit={handleSubmit}>
+          <div className="nombre-especialista">
+            <label htmlFor="nombreEspecialista">Nombre de la Compañia:</label>
+            <input
+              type="text"
+              id="nombreEspecialista" 
+              value={nombreEspecialista}
+              onChange={(e) => setNombreEspecialista(e.target.value)}
+              required
+            />
+          </div>
+          <div className="tipo-especialista"> 
+            <label htmlFor="tipoEspecialistal">Tipo de Especialista:</label>
+            <input
+            text="text"
+              id="tipoEspecialista"
+              value={tipoEspecialista}
+              onChange={(e) => setTipoEspecialista(e.target.value)}
+              required
+            >
+            </input>
+          </div>
+          <div className="tipo-documento-operador"> 
+            <label htmlFor="tipoDocumentoOperador">Tipo de Documento:</label>
+            <select
+              id="tipoDocumentoOperador"
+              value={tipoDocumentoOperador}
+              onChange={(e) => setTipoDocumentoOperador(e.target.value)}
+              required
+            >
+              <option value="">Selecciona un tipo</option>
+              <option value="Cedula de Ciudadania">Cedula de Ciudadania</option>
+              <option value="Tarjeta de Identidad">Tarjeta de Identidad</option>
+              <option value="Pasaporte">Pasaporte</option>
+              <option value="Carnet de Extranjería">Carnet de Extranjería</option>
+            </select>
+          </div>
+          <div className="numero-documento-especialista"> 
+            <label htmlFor="numeroDocumentoEspecialista">N° de Documento:</label>
+            <input
+              type="number"
+              id="numeroDocumentoEspecialista"
+              value={numeroDocumentoEspecialista}
+              onChange={(e) => setNumeroDocumentoEspecialista(e.target.value)}
+              required
+            />
+          </div>
+          <div className="valor-salarial">
+            <label htmlFor="valorSalarial">Valor Salarial x Hora:</label>
+            <input
+              type="number"
+              id="valorSalarial"
+              value={valorSalarial}
+              onChange={(e) => setValorSalarial(e.target.value)}
+              required
+            />
+          </div>
+          <button className="boton-especialista" type="submit">Crear Especialista</button>
+        </form>
+        </div>
+        <div className="formulario-operador"> 
+        <h3>Informacion del Operador</h3>
+        <form onSubmit={handleSubmit}>
+          <div className="nombre-operador">
+            <label htmlFor="nombreOperador">Nombre del Operador:</label>
+            <input
+              type="text"
+              id="nombreOperador" 
+              value={nombreOperador}
+              onChange={(e) => setNombreOperador(e.target.value)}
+              required
+            />
+          </div>
+          <div className="tipo-operador"> 
+            <label htmlFor="tipoOperador">Tipo de Especialista:</label>
+            <input
+            text="text"
+              id="tipoOperador"
+              value={tipoOperador}
+              onChange={(e) => setTipoOperador(e.target.value)}
+              required
+            >
+            </input>
+          </div>
+          <div className="tipo-documento-especialista"> 
+            <label htmlFor="tipoDocumentoEspecialista">Tipo de Documento:</label>
+            <select
+              id="tipoDocumentoEspecialista"
+              value={tipoDocumentoEspecialista}
+              onChange={(e) => setTipoDocumentoEspecialista(e.target.value)}
+              required
+            >
+              <option value="">Selecciona un tipo</option>
+              <option value="Cedula de Ciudadania">Cedula de Ciudadania</option>
+              <option value="Tarjeta de Identidad">Tarjeta de Identidad</option>
+              <option value="Pasaporte">Pasaporte</option>
+              <option value="Carnet de Extranjería">Carnet de Extranjería</option>
+            </select>
+          </div>
+          <div className="numero-documento-operador"> , 
+            <label htmlFor="numeroDocumentoOperador">N° de Documento:</label>
+            <input
+              type="number"
+              id="numeroDocumentoOperador"
+              value={numeroDocumentoOperador}
+              onChange={(e) => setNumeroDocumentoOperador(e.target.value)}
+              required
+            />
+          </div>
+          <div className="valor-salarial-operador"> , 
+            <label htmlFor="valorSalarialOperador">Valor Salarial x Hora:</label>
+            <input
+              type="number"
+              id="valorSalarialOperador"
+              value={valorSalarialOperador}
+              onChange={(e) => setValorSalarialOperador(e.target.value)}
+              required
+            />
+          </div>
+          <button className="boton-operador" type="submit">Crear Operador</button>
+        </form>
+        <button className="boton-guardar" type="submit">Guardar Usuario</button>
+        </div>
       </div>
     )
 }
